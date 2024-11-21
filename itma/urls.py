ed21553a3afe8bdec60c65a3ef00ad7532de71ca
Zrsion <1079451538@qq.com>
2024-11-21 16:40:04 +0800

    modify some files and update READEME.md

--- itma/urls.py
+++ itma/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
-from . import views
+from system import views as system_views
 from taskcalendar import views as taskcalendar_views
 from llmagent import views as llmagent_views
 from django.contrib.auth import views as auth_views
@@ -11,7 +11,10 @@ from django.urls import path
 urlpatterns = [
     path('', lambda request: redirect('login')),
     path('admin/', admin.site.urls),
-    path('home/', views.home, name='home'),
+    path('home/', system_views.home, name='home'),
+    path('login/', system_views.login_view, name='login'),
+    path('logout/', system_views.logout_view, name='logout'),
+    path('register/', system_views.register_view, name='register'),
     
     path('taskcalendar/', taskcalendar_views.taskcalendar, name='taskcalendar'),
     path('all_events/', taskcalendar_views.all_events, name='all_events'),
@@ -19,10 +22,6 @@ urlpatterns = [
     path('add_event/', taskcalendar_views.add_event, name='add_event'),
     path('update/', taskcalendar_views.update, name='update'),
     path('remove/', taskcalendar_views.remove, name='remove'),
-
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register_view, name='register'),
 
     path('llmagent/', llmagent_views.chatglm_view, name='llmagent'),
     path('set_api_key/', llmagent_views.set_api_key, name='set_api_key'), 
